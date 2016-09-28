967b8a960a63752878fc86803694bebeac305bb6
Robbie <robbie.schneider@triadadv.com>
2016-09-28 08:19:57 -0400

    Added Default index.php Content
    Just added the default WP Loop to the index.php file.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
-    <h1>Page Title</h1>            
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <?php the_title(); ?>
+    
+    <?php the_content(); ?>
+    
+<?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
